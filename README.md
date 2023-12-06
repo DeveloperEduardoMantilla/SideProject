e25a48e019be4a7ad3d815063ce8c13b164bd5d4
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-12-06 15:41:43 -0500

    feat(SideProject): :memo: Actualizacion ModeloEntidadRelacion y Script Db
    Se realiza actualizacion de esquema y script que contiene el codigo para la creacion de la base de datos en Mysql
    
    EduardoMantilla

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # SideProject
 
 <img src="./doc/db/EsquemaDB.png" />
+
+## Stack Tecnologico 
+
+<img src="./doc/StackTecnologico.png" />
+
+### Requerimientos funcionales
+
+1. La aplicacion debe contar con dos roles Administrativo - Camper
+
+Rol Administrativo
+1. Permitir crear nuevos usuarios.
+2. Permitir o denegear el acceso a la plataforma a usuarios que se registren por si mismos.
+3. Listar usuarios con sus cv cargadas en el sistema.
+4. Permitir editar la informacion de un cv de un camper
+5. Permitir si la cv de un camper se puede ver o no en la plataforma.
+6. Gestionar permiso a un camper para que pueda editar o no la plataforma.
+
+Rol Camper
+1. Registrarse en la plataforma.
+2. Cargar informacion requerida para poder tener un cv visto 
+
+
+EndPoints 
+CRUD - CV
+CRUD - USUARIO
+CRUD - EDUCACION
+CRUD - SOFTSKILLS
+CRUD - EXPERIENCIA 
+
+Tabla Cv - (Get)   
+    nombre  
+    foto
+    palabrasClave
+    idioma
+    nivel
--- doc/db/Script.sql
+++ doc/db/Script.sql
@@ -3,12 +3,13 @@ CREATE TABLE `experiencia`(
     `cargo` TEXT NOT NULL,
     `empresa` TEXT NOT NULL,
     `descripcionLogros` TEXT NOT NULL,
-    `fecha` TEXT NOT NULL
+    `fecha` TEXT NOT NULL,
+    `idCv` INT NOT NULL
 );
 ALTER TABLE
-    `experiencia` ADD PRIMARY KEY `experiencia_id_primary`(`id`);
+    `experiencia` ADD PRIMARY KEY(`id`);
 CREATE TABLE `cv`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `foto` TEXT NOT NULL,
     `nombre` TEXT NOT NULL,
     `palabrasClave` JSON NOT NULL,
@@ -16,73 +17,41 @@ CREATE TABLE `cv`(
     `skills` JSON NOT NULL,
     `idUsuario` INT NOT NULL,
     `idioma` TEXT NOT NULL,
-    `nivelIdioma` TEXT NOT NULL
-);
-ALTER TABLE
-    `cv` ADD PRIMARY KEY `cv_id_primary`(`id`);
-CREATE TABLE `contacto`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `telefono` TEXT NOT NULL,
-    `correo` TEXT NOT NULL,
+    `nivelIdioma` TEXT NOT NULL,
+    `estado` TINYINT(1) NOT NULL,
+    `accesoEditar` TINYINT(1) NOT NULL,
     `github` TEXT NOT NULL,
-    `linkedin` TEXT NOT NULL,
-    `ciudad` TEXT NOT NULL,
-    `idCv` INT NOT NULL
+    `linkedin` TEXT NOT NULL
 );
-ALTER TABLE
-    `contacto` ADD PRIMARY KEY `contacto_id_primary`(`id`);
-CREATE TABLE `competenciasCv`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `competenciasSocioEmocionalesId` INT NOT NULL,
-    `cvId` INT NOT NULL
-);
-ALTER TABLE
-    `competenciasCv` ADD PRIMARY KEY `competenciascv_id_primary`(`id`);
 CREATE TABLE `educacion`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `titulo` TEXT NOT NULL,
     `institucion` TEXT NOT NULL,
     `fecha` TEXT NOT NULL,
     `tipo` INT NOT NULL,
     `idCv` INT NOT NULL
 );
-ALTER TABLE
-    `educacion` ADD PRIMARY KEY `educacion_id_primary`(`id`);
-CREATE TABLE `experienciaCv`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `cvId` INT NOT NULL,
-    `experiencia` INT NOT NULL
-);
-ALTER TABLE
-    `experienciaCv` ADD PRIMARY KEY `experienciacv_id_primary`(`id`);
 CREATE TABLE `softSkills`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `compentencia` TEXT NOT NULL
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `compentencia` TEXT NOT NULL,
+    `idCv` INT NOT NULL
 );
-ALTER TABLE
-    `softSkills` ADD PRIMARY KEY `softskills_id_primary`(`id`);
 CREATE TABLE `usuario`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `usuario` TEXT NOT NULL,
     `password` TEXT NOT NULL,
     `fechaRegistro` TIMESTAMP NOT NULL,
-    `ultimoIngreso` TEXT NOT NULL,
-    `rol` INT NOT NULL,
+    `rol` TEXT NOT NULL,
     `genero` TEXT NOT NULL,
-    `estado` INT NOT NULL
+    `estado` INT NOT NULL,
+    `telefono` TEXT NOT NULL,
+    `correo` TEXT NOT NULL,
+    `ciudad` TEXT NOT NULL
 );
 ALTER TABLE
-    `usuario` ADD PRIMARY KEY `usuario_id_primary`(`id`);
+    `experiencia` ADD CONSTRAINT `experiencia_idcv_foreign` FOREIGN KEY(`idCv`) REFERENCES `cv`(`id`);
 ALTER TABLE
-    `contacto` ADD CONSTRAINT `contacto_idcv_foreign` FOREIGN KEY(`idCv`) REFERENCES `cv`(`id`);
-ALTER TABLE
-    `competenciasCv` ADD CONSTRAINT `competenciascv_cvid_foreign` FOREIGN KEY(`cvId`) REFERENCES `cv`(`id`);
-ALTER TABLE
-    `experienciaCv` ADD CONSTRAINT `experienciacv_experiencia_foreign` FOREIGN KEY(`experiencia`) REFERENCES `experiencia`(`id`);
-ALTER TABLE
-    `experienciaCv` ADD CONSTRAINT `experienciacv_cvid_foreign` FOREIGN KEY(`cvId`) REFERENCES `cv`(`id`);
-ALTER TABLE
-    `competenciasCv` ADD CONSTRAINT `competenciascv_competenciassocioemocionalesid_foreign` FOREIGN KEY(`competenciasSocioEmocionalesId`) REFERENCES `softSkills`(`id`);
+    `softSkills` ADD CONSTRAINT `softskills_idcv_foreign` FOREIGN KEY(`idCv`) REFERENCES `cv`(`id`);
 ALTER TABLE
     `cv` ADD CONSTRAINT `cv_idusuario_foreign` FOREIGN KEY(`idUsuario`) REFERENCES `usuario`(`id`);
 ALTER TABLE
