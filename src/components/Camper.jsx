33859eefbd36aabfe6f5d67fe96394cdd1e53b21
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-12-13 20:22:24 -0500

    feat(SideProject): :construction: Trabajando en la construccion del componente de campers
    EduardoMantilla

--- src/components/Camper.jsx
+++ src/components/Camper.jsx
@@ -2,63 +2,73 @@
 
 import { Box, Typography } from "@mui/material";
 import photo from "../assets/Img/Avatar.png";
+import avatarMujer from "../assets/Img/AvatarMujer.png";
 
 export default function Camper(props) {
-
-  const {data, color} = props;
+  const { data, color } = props;
 
   const skillsToShow = data.skills.slice(0, 5);
   return (
     <Box className="camper_card">
-        <Box className="camper_img" sx={{background:color}}>
-          <img
-            src={photo}
+      <Box
+        className="camper_img"
+        sx={{ background: color }}
+      >
+        {
+          (data.genero ==="Masculino") ?  <img
+          src={photo}
+          alt=""
+          className="camper_Img"
+            /> : <img
+            src={avatarMujer}
             alt=""
             className="camper_Img"
           />
+        }
+        
+      </Box>
+      <Box className="camper_data">
+        <Box className="caja_informacion">
+          <Typography
+            variant="h5"
+            className="name"
+          >
+            {data.nombre}
+          </Typography>
+          <Typography
+            variant=""
+            className="nivelIngles"
+          >
+            Developer BackEnd
+          </Typography>
         </Box>
-        <Box className="camper_data"> 
-          <Box className="caja_informacion">
-            <Typography
-              variant="h5"
-              className="name"
-            >
-              {data.nombre}
-            </Typography>
-            <Typography
-              variant=""
-              className="nivelIngles"
-            >
-              Developer BackEnd
-            </Typography>
-          </Box>
-          
-          <Box className="caja_habilidades">
-            <Typography
-              variant=""
-              className="nivel_Ingles"
-            >
-              Nivel Ingles: {data.nivelIngles}
-            </Typography>
-            
-            <Typography
-              variant=""
-              className="skills"
-            >
-              <Box className="caja_Skills">
-                {skillsToShow.map((item, index) => (
-                  <span
-                    key={index}
-                    style={{background:"#ccc"}}
-                    className="skillsCamper"
-                  >
-                    {item}
-                  </span>
-                ))}
-              </Box> 
-            </Typography>
-          </Box>
+
+        <Box className="caja_habilidades">
+          <Typography
+            variant=""
+            className="nivel_Ingles"
+          >
+            Nivel Ingles: {data.nivelIngles}
+          </Typography>
+
+          <Typography
+            variant=""
+            className="skills"
+          >
+            <Box className="caja_Skills">
+              {skillsToShow.map((item, index) => (
+                <span
+                  key={index}
+                  style={{ background: "#ccc" }}
+                  className="skillsCamper"
+                >
+                  {item}
+                </span>
+              ))}
+            </Box>
+          </Typography>
         </Box>
+      </Box>
     </Box>
   );
 }
--- src/pages/public/Campers.jsx
+++ src/pages/public/Campers.jsx
@@ -8,7 +8,7 @@ import {
   Typography,
 } from "@mui/material";
 import Camper from "../../components/Camper.jsx";
-import figura from "../../assets/Img/cuadrados.png";
+import figura from "../../assets/Img/figura.png";
 import { useState, useEffect } from "react";
 import fondoCampers from "../../assets/Img/fondoCampers.svg";
 import "../../assets/css/Camper.css";
@@ -44,110 +44,137 @@ export default function Campers() {
   return (
     <>
       <Box
-        sx={{
-          backgroundImage: `url(${fondoCampers})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat:"no-repeat",
-          backgroundAttachment:"local",
-          position:"relative",
-          padding:"30px 0"
-        }}
+      sx={{position:"relative", padding:"60px 0 120px 0"}}
+      // sx={{
+      //   backgroundImage: `url(${fondoCampers})`,
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      //   backgroundRepeat:"no-repeat",
+      //   backgroundAttachment:"local",
+      //   position:"relative",
+      //   padding:"30px 0"
+      // }}
       >
         <Container>
-        <Typography
-          sx={{
-            fontSize: "60px",
-            fontWeight: "600",
-            paddingBottom: "10px",
-            color: "#fff",
-          }}
-        >
-          Campers
-        </Typography>
-        <Box>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
+          <Typography
             sx={{
-              marginRight: "20px",
-              fontSize: "14px",
-              width: { xs: "100%", md: "300px" },
-              padding: "0",
-              color:"#fff"
+              fontSize: "60px",
+              fontWeight: "600",
+              paddingBottom: "10px",
+              color: "#333333",
             }}
-            value={tecnologia}
-            label="Age"
-            onChange={handleChangeTecnologia}
           >
-            <MenuItem value={1}>Tecnologias</MenuItem>
-            <MenuItem value={2}>Php</MenuItem>
-            <MenuItem value={3}>React</MenuItem>
-            <MenuItem value={4}>Java</MenuItem>
-          </Select>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            sx={{
-              fontSize: "14px",
-              width: { xs: "100%", md: "300px" },
-              margin: { xs: "15px 0", sm: "0" },
-              marginRight: { sm: "20px" },
-              padding: "0",
-              color:"#fff"
-            }}
-            value={ruta}
-            label="Age"
-            onChange={handleChangeRuta}
-          >
-            <MenuItem value={5}>Ruta</MenuItem>
-            <MenuItem value={6}>Developer BackEnd</MenuItem>
-            <MenuItem value={7}>Developer FullStack</MenuItem>
-            <MenuItem value={8}>Develope FrontEnd</MenuItem>
-          </Select>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            sx={{
-              fontSize: "14px",
-              width: { xs: "100%", md: "300px" },
-              padding: "0",
-              color:"#fff"
-            }}
-            value={ingles}
-            label="Age"
-            onChange={handleChangeIngles}
-          >
-            <MenuItem value={9}>Nivel ingles</MenuItem>
-            <MenuItem value={10}>A1</MenuItem>
-            <MenuItem value={11}>A2</MenuItem>
-            <MenuItem value={12}>B1</MenuItem>
-          </Select>
-        </Box>
-        <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-          {
-            data ? (
-            data.map((item) => (
-              <Camper
-                data={item}
-                key={item.nombre}
-                color="#ccc"
-              />
-            ))
-          ) : (
-            <p>Cargando datos...</p>
-          )}
-        </Box>
+            Campers
+          </Typography>
+          <Box>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              sx={{
+                marginRight: "20px",
+                fontSize: "14px",
+                width: { xs: "100%", md: "300px" },
+                padding: "0",
+                color: "#fff",
+              }}
+              value={tecnologia}
+              label="Age"
+              onChange={handleChangeTecnologia}
+            >
+              <MenuItem value={1}>Tecnologias</MenuItem>
+              <MenuItem value={2}>Php</MenuItem>
+              <MenuItem value={3}>React</MenuItem>
+              <MenuItem value={4}>Java</MenuItem>
+            </Select>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              sx={{
+                fontSize: "14px",
+                width: { xs: "100%", md: "300px" },
+                margin: { xs: "15px 0", sm: "0" },
+                marginRight: { sm: "20px" },
+                padding: "0",
+                color: "#fff",
+              }}
+              value={ruta}
+              label="Age"
+              onChange={handleChangeRuta}
+            >
+              <MenuItem value={5}>Ruta</MenuItem>
+              <MenuItem value={6}>Developer BackEnd</MenuItem>
+              <MenuItem value={7}>Developer FullStack</MenuItem>
+              <MenuItem value={8}>Develope FrontEnd</MenuItem>
+            </Select>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              sx={{
+                fontSize: "14px",
+                width: { xs: "100%", md: "300px" },
+                padding: "0",
+                color: "#fff",
+              }}
+              value={ingles}
+              label="Age"
+              onChange={handleChangeIngles}
+            >
+              <MenuItem value={9}>Nivel ingles</MenuItem>
+              <MenuItem value={10}>A1</MenuItem>
+              <MenuItem value={11}>A2</MenuItem>
+              <MenuItem value={12}>B1</MenuItem>
+            </Select>
+          </Box>
+          <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+            {data ? (
+              data.map((item) => (
+                <Camper
+                  data={item}
+                  key={item.nombre}
+                  color={item.genero=="Masculino"?"#2A4B9B":"#6C3483"}
+                />
+              ))
+            ) : (
+              <p>Cargando datos...</p>
+            )}
+          </Box>
         </Container>
-        {/* <img
+        <img
           src={figura}
           style={{
             position: "absolute",
-            top: "0",
-            right: "-10px",
+            top: "30%",
+            bottom:"0px",
+            right:"90px",
             zIndex: "1",
+            width:"100px"
+            
           }}
-        /> */}
+        />
+        <img
+          src={figura}
+          style={{
+            position: "absolute",
+            top: "50%",
+            bottom:"90px",
+            left:"90px",
+            zIndex: "1",
+            width:"100px"
+            
+          }}
+        />
+        <img
+          src={figura}
+          style={{
+            position: "absolute",
+            top: "90%",
+            bottom:"0px",
+            right:"250px",
+            zIndex: "1",
+            width:"100px"
+            
+          }}
+        />
       </Box>
     </>
   );
